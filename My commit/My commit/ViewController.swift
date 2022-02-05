c21a7a1dbc92d91b7e1b3963bdbe4afafb59b43b
Oleksii Kutsevol <>
2022-02-05 16:57:20 +0200

    add comment

--- My commit/My commit/ViewController.swift
+++ My commit/My commit/ViewController.swift
@@ -9,6 +9,8 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    
+    view.backgroundColor = .white
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
